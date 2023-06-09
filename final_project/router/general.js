3fc990026c72be7b4b2a30594f9fcad3c25cce19
CaelSerrao <86877852+CaelSerrao@users.noreply.github.com>
2023-06-09 11:26:08 +0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,14 +32,30 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    res.send(books[author])
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      }
+    });
+    res.send(JSON.stringify({booksbyauthor}, null, 4));
 });
-
+ 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    res.send(books[title])
+    let booksbytitle = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        booksbytitle.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      }
+    });
+    res.send(JSON.stringify({booksbytitle}, null, 4));
 });
 
 //  Get book review
@@ -48,4 +64,72 @@ public_users.get('/review/:isbn',function (req, res) {
     res.send(books[isbn]['reviews'])
 });
 
+public_users.get('/async-booklist',function (req, res) {    
+    const bookslist = new Promise((resolve, reject) => {        
+          resolve(res.send(JSON.stringify({books}, null, 4)));      
+        });      
+          booklist.then(() => console.log("Task 10 promise resolved"));  
+});
+public_users.get('/async-booklist',function (req, res) {    
+    const bookslist = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;     
+          resolve(res.send(books[isbn]));      
+        });      
+          booklist.then(() => console.log("Task 10 promise resolved"));  
+});
+
+public_users.get('/async-booklistbyauthor',function(req,res){
+    const getbooksbyauthor = new Promise((resolve, reject) => {
+
+        let booksbyauthor = [];
+        let isbns = Object.keys(books);
+        isbns.forEach((isbn) => {
+          if(books[isbn]["author"] === req.params.author) {
+            booksbyauthor.push({"isbn":isbn,
+                                "title":books[isbn]["title"],
+                                "reviews":books[isbn]["reviews"]});
+          resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+          }
+    
+    
+        });
+        reject(res.send("The mentioned author does not exist "))
+            
+        });
+    
+        getbooksbyauthor.then(function(){
+                console.log("Promise is resolved");
+       }).catch(function () { 
+                    console.log('The mentioned author does not exist');
+      });
+});
+
+public_users('/async-booklistbytitle', function(req,res){
+    const getbooksbytitle = new Promise((resolve, reject) => {
+
+        let booksbytitle = [];
+        let isbns = Object.keys(books);
+        isbns.forEach((isbn) => {
+          if(books[isbn]["author"] === req.params.author) {
+            booksbytitle.push({"isbn":isbn,
+                                "title":books[isbn]["title"],
+                                "reviews":books[isbn]["reviews"]});
+          resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+          }
+    
+    
+        });
+        reject(res.send("The mentioned author does not exist "))
+            
+        });
+    
+        getbooksbytitle.then(function(){
+                console.log("Promise is resolved");
+       }).catch(function () { 
+                    console.log('The mentioned author does not exist');
+      });
+});
+  
+  
 module.exports.general = public_users;
+
